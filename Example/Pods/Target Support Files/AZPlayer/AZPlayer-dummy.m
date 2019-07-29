61c72d8b64ae7714787499bfb02cb516597ca5f7
Alex Zbirnik <alxzorg@gmail.com>
2019-07-29 14:03:00 +0300

    Initial Commit

---
+++ Example/Pods/Target Support Files/AZPlayer/AZPlayer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AZPlayer : NSObject
+@end
+@implementation PodsDummy_AZPlayer
+@end
---
+++ Example/Pods/Target Support Files/Pods-AZPlayer_Example/Pods-AZPlayer_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AZPlayer_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AZPlayer_Example
+@end
